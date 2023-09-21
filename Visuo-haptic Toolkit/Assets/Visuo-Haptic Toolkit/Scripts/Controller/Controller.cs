55e5ee1af9fbf944216fa02342664b7d539e14a7
benoitgeslain <benoitgeslain@gmail.com>
2023-09-21 10:42:58 +0200

    Added mouse controller

--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Controller/Controller.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Controller/Controller.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour {
 
 	public float speed = 0.5f;
 
+	Camera camera;
+	Vector3 previousMousePosition;
+	Transform clickedObject;
+
+	void Awake() {
+		camera = Camera.main;
+	}
+
 	void Update() {
+		// KEYBOARD
         // Get the horizontal and vertical axis.
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
@@ -13,5 +23,16 @@ public class Controller : MonoBehaviour {
 
         // Move translation along the object's z-axis
         transform.Translate(translationHorizontal, 0, translationVertical);
+
+
+		// MOUSE
+		Mouse mouse = Mouse.current;
+		// If left clic mouse button is pressed, apply the mouse displacement converted from camera space to world space to the transform
+		if (mouse.leftButton.isPressed) {
+			Vector3 mousePosition = mouse.position.ReadValue();
+
+			transform.position += camera.ScreenToWorldPoint(mousePosition) - camera.ScreenToWorldPoint(previousMousePosition);
+		}
+		previousMousePosition = mouse.position.ReadValue();
     }
 }
