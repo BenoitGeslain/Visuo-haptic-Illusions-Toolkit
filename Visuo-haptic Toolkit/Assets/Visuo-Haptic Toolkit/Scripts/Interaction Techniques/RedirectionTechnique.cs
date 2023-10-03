224e8d69773bd010a3d1f5833a83c69d250c828a
Bruno Jartoux <bruno.jartoux@sii.fr>
2023-10-03 15:43:02 +0200

    add RedirectionTechnique class

---
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Interaction Techniques/RedirectionTechnique.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BG.Redirection {
+    abstract public class RedirectionTechnique {
+        /// <summary>
+        /// This virtual function applies the redirection to the Scene transforms according to the other parameters and the equations
+        /// defined in the corresponding techniques. It needs to be overriden by a child class and it is called on Update() in *Redirection classes.
+        /// </summary>
+        public virtual void Redirect(Scene scene) {
+            Debug.LogError("Calling Redirect() virtual method. It should be overriden");
+        }
+    }
+}
--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Controller/Controller.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Controller/Controller.cs
@@ -10,17 +10,15 @@ namespace BG.Visualisation {
 		Vector3 previousMousePosition;
 		bool wasFocused;
 
-		void Awake() {
-			cam = Camera.main;
-		}
+        void Awake() => cam = Camera.main;
 
-		void Update() {
+        void Update() {
 			// KEYBOARD
 			// Get the horizontal and vertical axis.
 			// By default they are mapped to the arrow keys.
 			// The value is in the range -1 to 1
 			float translationVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-			float translationHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            float translationHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
 			// Move translation along the object's z-axis
 			transform.Translate(translationHorizontal, 0, translationVertical);
