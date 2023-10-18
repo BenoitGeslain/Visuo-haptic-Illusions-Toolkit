6f3e0fa8dae2b3c3a9633f64aae5b19d7d5b44f8
Bruno Jartoux <bruno.jartoux@sii.fr>
2023-10-18 10:49:26 +0200

    start implementing min jerk trajectory

---
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Logging/UserTrajectorySimulation/MinJerkTrajectory.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+// TODO fix namespace issues
+
+namespace Assets.Visuo_Haptic_Toolkit.Scripts.Logging.UserTrajectorySimulation {
+
+
+    public class MinJerkTrajectory {
+        /// <summary>
+        /// Minimum Jerk Trajectory (L2 norm) between the input points from t=0 to t=1 with boundary conditions speed = acceleration = 0.
+        /// </summary>
+        /// <param name="pos0"></param>
+        /// <param name="pos1"></param>
+        /// <returns></returns>
+        static System.Func<float, Vector3> minJerkTrajectory(Vector3 pos0, Vector3 pos1) {
+            float[] coeffsByIncreasingPower = { 0, 0, 0, 10, -15, 6 };
+            return t => pos0 + (pos1 - pos0) * (float)coeffsByIncreasingPower.Select((a, i) => a * Mathf.Pow(t, i)).Sum();
+        }
+    }
+}
