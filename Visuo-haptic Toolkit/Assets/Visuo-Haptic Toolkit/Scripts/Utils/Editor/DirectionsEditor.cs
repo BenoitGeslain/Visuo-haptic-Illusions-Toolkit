2d18a71106c593277acd14c33c6f68911b0a2f6d
benoit.geslain <benoit.geslain@sii.fr>
2023-10-21 12:36:33 +0200

    ParametersToolkit zenner values

--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Editor/DirectionsEditor.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Editor/DirectionsEditor.cs
@@ -3,8 +3,35 @@ using UnityEditor;
 
 namespace BG.Redirection {
 	[CanEditMultipleObjects]
-	[CustomPropertyDrawer(typeof(Vector2Translation))]
-	public class Vector2TranslationPropertyDrawer: PropertyDrawer {
+	[CustomPropertyDrawer(typeof(Vector2Horizontal))]
+	public class Vector2HorizontalPropertyDrawer: PropertyDrawer {
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+			EditorGUI.BeginProperty(position, label, property);
+
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+			// Don't indent child fields
+			var indent = EditorGUI.indentLevel;
+			EditorGUIUtility.labelWidth = 60;
+			EditorGUI.indentLevel = 0;
+
+			var leftRect = new Rect(position.x, position.y, position.width / 2, position.height);
+			var rightRect = new Rect(position.x + position.width / 2, position.y, position.width / 2, position.height);
+
+			EditorGUI.PropertyField(leftRect, property.FindPropertyRelative("left"), new GUIContent("Left"));
+			EditorGUI.PropertyField(rightRect, property.FindPropertyRelative("right"), new GUIContent("Right"));
+
+			EditorGUI.indentLevel = indent;
+			EditorGUIUtility.labelWidth = 0;
+
+			EditorGUI.EndProperty();
+		}
+	}
+
+
+	[CanEditMultipleObjects]
+	[CustomPropertyDrawer(typeof(Vector2Vertical))]
+	public class Vector2VerticalPropertyDrawer: PropertyDrawer {
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
 			EditorGUI.BeginProperty(position, label, property);
 
@@ -18,8 +45,35 @@ namespace BG.Redirection {
 			var minRect = new Rect(position.x, position.y, position.width / 2, position.height);
 			var maxRect = new Rect(position.x + position.width / 2, position.y, position.width / 2, position.height);
 
-			EditorGUI.PropertyField(maxRect, property.FindPropertyRelative("forward"), new GUIContent("Forward"));
-			EditorGUI.PropertyField(minRect, property.FindPropertyRelative("backward"), new GUIContent("Backward"));
+			EditorGUI.PropertyField(minRect, property.FindPropertyRelative("down"), new GUIContent("Down"));
+			EditorGUI.PropertyField(maxRect, property.FindPropertyRelative("up"), new GUIContent("Up"));
+
+			EditorGUI.indentLevel = indent;
+			EditorGUIUtility.labelWidth = 0;
+
+			EditorGUI.EndProperty();
+		}
+	}
+
+
+	[CanEditMultipleObjects]
+	[CustomPropertyDrawer(typeof(Vector2Gain))]
+	public class Vector2GainPropertyDrawer: PropertyDrawer {
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+			EditorGUI.BeginProperty(position, label, property);
+
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+			// Don't indent child fields
+			var indent = EditorGUI.indentLevel;
+			EditorGUIUtility.labelWidth = 60;
+			EditorGUI.indentLevel = 0;
+
+			var minRect = new Rect(position.x, position.y, position.width / 2, position.height);
+			var maxRect = new Rect(position.x + position.width / 2, position.y, position.width / 2, position.height);
+
+			EditorGUI.PropertyField(minRect, property.FindPropertyRelative("slower"), new GUIContent("Slower"));
+			EditorGUI.PropertyField(maxRect, property.FindPropertyRelative("faster"), new GUIContent("Faster"));
 
 			EditorGUI.indentLevel = indent;
 			EditorGUIUtility.labelWidth = 0;
