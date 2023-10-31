60a88d3ae76383359fc713bfbe53f5c04870ee1a
benoitgeslain <benoitgeslain@gmail.com>
2023-10-31 13:28:57 +0100

    temporary BR Editor

--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Editor/Redirection/BodyRedirectionEditor.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Editor/Redirection/BodyRedirectionEditor.cs
@@ -13,11 +13,34 @@ namespace BG.Redirection {
 		SerializedProperty techniqueInstance;
 
 		SerializedProperty scene;
+		SerializedProperty physicalHand;
+		SerializedProperty virtualHand;
+		SerializedProperty physicalHead;
+		SerializedProperty virtualHead;
+		SerializedProperty physicalTarget;
+		SerializedProperty virtualTarget;
+		SerializedProperty origin;
+		SerializedProperty targetsScene;
+		SerializedProperty radius;
+		SerializedProperty applyDampening;
+		SerializedProperty applySmoothing;
 
 		private void OnEnable() {
 			technique = serializedObject.FindProperty("technique");
             techniqueInstance = serializedObject.FindProperty("techniqueInstance");
+
 			scene = serializedObject.FindProperty("scene");
+			physicalHand = serializedObject.FindProperty("scene.physicalHand");
+			virtualHand = serializedObject.FindProperty("scene.virtualHand");
+			physicalHead = serializedObject.FindProperty("scene.physicalHead");
+			virtualHead = serializedObject.FindProperty("scene.virtualHead");
+			physicalTarget = serializedObject.FindProperty("scene.physicalTarget");
+			virtualTarget = serializedObject.FindProperty("scene.virtualTarget");
+			origin = serializedObject.FindProperty("scene.origin");
+			targetsScene = serializedObject.FindProperty("scene.targets");
+			radius = serializedObject.FindProperty("scene.radius");
+			applyDampening = serializedObject.FindProperty("scene.applyDampening");
+			applySmoothing = serializedObject.FindProperty("scene.applySmoothing");
 		}
 
 		public override void OnInspectorGUI() {
@@ -29,7 +52,11 @@ namespace BG.Redirection {
 			if (technique.enumNames[technique.enumValueIndex] == "Geslain2022Polynom") {
 				EditorGUILayout.PropertyField(techniqueInstance, new GUIContent ("Parameters"));
 			}
-			EditorGUILayout.PropertyField(scene, new GUIContent("Scene"));
+
+			// Scene
+			// EditorGUILayout.PropertyField(scene, new GUIContent("Scene"));
+			EditorGUILayout.PropertyField(physicalHand, new GUIContent("Physical Hand"));
+			EditorGUILayout.PropertyField(physicalHand, new GUIContent("Virtual Hand"));
 
 			serializedObject.ApplyModifiedProperties();
 		}
