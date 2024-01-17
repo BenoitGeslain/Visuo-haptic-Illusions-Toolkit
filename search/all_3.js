1c6095cf96a7e0a2bf963195194e6c7482f4ffc1
BenoitGeslain <benoitgeslain@gmail.com>
2024-01-17 14:26:58 +0000

    Deploying to gh-pages from @ BenoitGeslain/Visuo-haptic-Illusions-Toolkit@1ea9f8c65093f2fab77236f874438fd315f0f953 🚀

--- search/all_3.js
+++ search/all_3.js
@@ -7,7 +7,7 @@ var searchData=
   ['controllerbr_2ecs_32',['ControllerBR.cs',['../ControllerBR_8cs.html',1,'']]],
   ['controllerwr_2ecs_33',['ControllerWR.cs',['../ControllerWR_8cs.html',1,'']]],
   ['controlpoint_34',['controlPoint',['../classVHToolkit_1_1Redirection_1_1BodyRedirectionTechnique.html#a44f6907bb17ec8dcd7332f72222d382a',1,'VHToolkit::Redirection::BodyRedirectionTechnique']]],
-  ['copyheadandhandtransform_35',['copyHeadAndHandTransform',['../classVHToolkit_1_1Redirection_1_1WorldRedirectionTechnique.html#a342b5da9c7fba525a7cdbcd194fda6be',1,'VHToolkit::Redirection::WorldRedirectionTechnique']]],
+  ['copyheadandhandtransform_35',['CopyHeadAndHandTransform',['../classVHToolkit_1_1Redirection_1_1WorldRedirectionTechnique.html#a5773d256bdbd3ca962ae1b207033dd02',1,'VHToolkit::Redirection::WorldRedirectionTechnique']]],
   ['createnewfile_36',['createNewFile',['../classVHToolkit_1_1Logging_1_1Logging.html#a03eb1d51c841950ae9e3c9af2d18dd83',1,'VHToolkit::Logging::Logging']]],
   ['curvatureradius_37',['CurvatureRadius',['../classVHToolkit_1_1Redirection_1_1ParametersToolkit.html#a094b9a6beb61e1b1ae42f33a87db3091',1,'VHToolkit::Redirection::ParametersToolkit']]],
   ['curvatureradiustorotationrate_38',['CurvatureRadiusToRotationRate',['../classVHToolkit_1_1Redirection_1_1Toolkit.html#a50a9ffa9ce5e74ecba6bb009db7e8cad',1,'VHToolkit.Redirection.Toolkit.CurvatureRadiusToRotationRate()'],['../classVHToolkit_1_1Redirection_1_1Toolkit.html#ad43190700bbeb326a95bb5d5951a7469',1,'VHToolkit.Redirection.Toolkit.CurvatureRadiusToRotationRate(float radius)']]]
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['calibrate_333',['Calibrate',['../classSceneCalibration.html#a6c54279a53a78b159cfa349f1cc0e773',1,'SceneCalibration']]],
-  ['copyheadandhandtransform_334',['copyHeadAndHandTransform',['../classVHToolkit_1_1Redirection_1_1WorldRedirectionTechnique.html#a342b5da9c7fba525a7cdbcd194fda6be',1,'VHToolkit::Redirection::WorldRedirectionTechnique']]],
+  ['copyheadandhandtransform_334',['CopyHeadAndHandTransform',['../classVHToolkit_1_1Redirection_1_1WorldRedirectionTechnique.html#a5773d256bdbd3ca962ae1b207033dd02',1,'VHToolkit::Redirection::WorldRedirectionTechnique']]],
   ['createnewfile_335',['createNewFile',['../classVHToolkit_1_1Logging_1_1Logging.html#a03eb1d51c841950ae9e3c9af2d18dd83',1,'VHToolkit::Logging::Logging']]],
   ['curvatureradiustorotationrate_336',['CurvatureRadiusToRotationRate',['../classVHToolkit_1_1Redirection_1_1Toolkit.html#a50a9ffa9ce5e74ecba6bb009db7e8cad',1,'VHToolkit.Redirection.Toolkit.CurvatureRadiusToRotationRate()'],['../classVHToolkit_1_1Redirection_1_1Toolkit.html#ad43190700bbeb326a95bb5d5951a7469',1,'VHToolkit.Redirection.Toolkit.CurvatureRadiusToRotationRate(float radius)']]]
 ];
