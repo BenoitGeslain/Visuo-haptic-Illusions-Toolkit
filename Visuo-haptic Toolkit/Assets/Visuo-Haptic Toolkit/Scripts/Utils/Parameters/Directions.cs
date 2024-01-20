b695b36c2f7cdc41158266ac6cb7880ccdee5e28
bjrtx <57685415+bjrtx@users.noreply.github.com>
2024-01-20 19:07:48 +0100

    Document classes

--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Parameters/Directions.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Parameters/Directions.cs
@@ -1,22 +1,36 @@
 using UnityEngine;
 
 namespace VHToolkit.Redirection {
+	/// <summary>
+	/// Wrapper for a pair of <c>float</c> associated with left and right.
+	/// </summary>
 	[System.Serializable]
 	public class Vector2Horizontal {
 		public float left;
 		public float right;
 	}
+
+	/// <summary>
+	/// Wrapper for a pair of <c>float</c> associated with up and down.
+	/// </summary>
 	[System.Serializable]
 	public class Vector2Vertical {
 		public float up;
 		public float down;
 	}
+
+	/// <summary>
+	/// Wrapper for a pair of <c>float</c> associated with fast and slow.
+	/// </summary>
 	[System.Serializable]
 	public class Vector2Gain {
 		public float faster;
 		public float slower;
 	}
 
+	/// <summary>
+	/// Wrapper for a pair of <c>float</c> associated with same and opposite.
+	/// </summary>
 	[System.Serializable]
 	public class Vector2Rotation {
 		public float same;
