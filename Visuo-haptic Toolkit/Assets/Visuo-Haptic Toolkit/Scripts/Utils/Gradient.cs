948d7dbc4299ff89d5ca6979febda9675eada7b5
Bruno Jartoux <57685415+bjrtx@users.noreply.github.com>
2024-02-01 14:28:20 +0100

    Add MathNet dll (#31)
    * Implement Inverse Kinematics
    
    * Add MathNet.Numerics dll
    
    * Implement 3D thin-plate splines

--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Gradient.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/Gradient.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.U2D.IK;
-using UnityEngine.UIElements;
 
 namespace VHToolkit {
     public static class MathTools {
