02b7088e6c9d200678581ae8c7628a105c2af372
benoitgeslain <benoitgeslain@gmail.com>
2024-02-20 09:36:22 +0100

    fixed splines.cs

--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/ThinPlateSpline.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Utils/ThinPlateSpline.cs
@@ -6,8 +6,7 @@ using MathNet.Numerics.LinearAlgebra;
 using System.Collections.Generic;
 
 
-namespace VHToolkit
-{
+namespace VHToolkit {
     public static class ThinPlateSpline {
         // The notation follows [Eberly 96, Thin-Plate Splines] at https://www.geometrictools.com/Documentation/ThinPlateSplines.pdf.
         static float GreenFunction2D(float distance) => distance * distance * MathF.Log(distance);
@@ -102,22 +101,6 @@ namespace VHToolkit
             var components = Enumerable.Range(0, 3).Select(i => SolveSmoothed(x, Array.ConvertAll(y, yy => yy[i]), lambda)).ToArray();
 
             return point => new(components[0](point), components[1](point), components[2](point));
-        }
-
-        public static Func<Vector3, float> Solve(Vector3[] x, float[] y) => SolveSmoothed(x, y, 0f);
-
-        public static Func<Vector3, float> SolveSmoothed(Vector3[] x, float[] y, float lambda)
-        {
-            static float GreenFunction3D(Vector3 a, Vector3 b) => Vector3.Distance(a, b);
-            var Mat = Matrix<float>.Build;
-
-            var yVec = Vector<float>.Build.DenseOfArray(y);
-            var N = Mat.DenseOfRowArrays(x.Select(xx => new[] { 1, xx.x, xx.y, xx.z }));
-            var Minv = (Mat.Dense(x.Length, x.Length, (i, j) => GreenFunction3D(x[i], x[j])) + lambda * Mat.DenseIdentity(x.Length)).Inverse();
-            var b = (N.Transpose() * Minv * N).Inverse() * N.Transpose() * Minv * yVec;
-            var w = Minv * (yVec - N * b);
-
-            return (point) => x.Select(xx => GreenFunction3D(xx, point)).Zip(w, (ww, g) => ww * g).Sum() + b[0] + b[1] * point.x + b[2] * point.y + b[3] * point.z;
         }
     }
 }
