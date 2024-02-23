7bec35a4e7edc71926681ae1be59125646ca1314
benoitgeslain <benoitgeslain@gmail.com>
2024-02-23 14:19:30 +0100

    Start implementing JSON logging

--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Logging/Logging.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Logging/Logging.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Globalization;
+using System.Text.Json;
 
 using CsvHelper;
 using CsvHelper.Configuration;
@@ -11,6 +12,7 @@ using UnityEngine;
 using VHToolkit.Redirection;
 using VHToolkit.Redirection.BodyRedirection;
 using VHToolkit.Redirection.WorldRedirection;
+using Newtonsoft.Json;
 
 namespace VHToolkit.Logging
 {
@@ -70,7 +72,7 @@ namespace VHToolkit.Logging
 	}
 
 	/// <summary>
-	/// This class handles the logging behavior at execution time.
+	/// This class handles the CSV logging behavior at execution time.
 	/// </summary>
 	public class Logging : MonoBehaviour {
 
@@ -119,4 +121,51 @@ namespace VHToolkit.Logging
 			writeHeaders<RedirectionData, RedirectionDataMap>(out records);
 		}
     }
+
+	public class JsonLogging : MonoBehaviour
+	{
+
+		public string pathToFile = "LoggedData\\";
+		[SerializeField] private string fileNamePrefix;
+		private string fileName;
+		private readonly int bufferSize = 10; // number of records kept before writing to disk
+
+		private List<RedirectionData> records = new();
+		private readonly CsvConfiguration config = new(CultureInfo.InvariantCulture) { HasHeaderRecord = false, MemberTypes = MemberTypes.Fields };
+
+		private Interaction script;
+
+		private void Start()
+		{
+			CreateNewFile();
+			script = GetComponent<Interaction>();
+		}
+
+		private void Update()
+		{
+			void writeRecords<Data, DataMap>(List<Data> records) where DataMap : ClassMap<Data>
+			{
+				if (records.Count > bufferSize)
+				{
+					using var writer = new StreamWriter(fileName, append: true);
+					using var csv = new CsvWriter(writer, config);
+					foreach (var record in records) {
+						writer.WriteLine(JsonConvert.SerializeObject(record));
+					}
+					records.Clear();
+				}
+			}
+
+			records.Add(new RedirectionData(script));
+			writeRecords<RedirectionData, RedirectionDataMap>(records);
+		}
+
+		public void CreateNewFile()
+		{
+			fileName = $"{pathToFile}{fileNamePrefix}{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.json";
+			using StreamWriter writer = new(fileName);
+			using CsvWriter csv = new(writer, CultureInfo.InvariantCulture);
+			records = new List<RedirectionData>();
+		}
+	}
 }
