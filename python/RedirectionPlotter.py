d0a983e2113ee78b75188a98fac8c831e218d91b
benoitgeslain <benoitgeslain@gmail.com>
2024-02-29 17:19:16 +0100

    legend and title

--- python/RedirectionPlotter.py
+++ python/RedirectionPlotter.py
@@ -10,10 +10,10 @@ plt.ion() # turn on interactive mode
 fig = plt.figure(figsize=(10,6), constrained_layout=True)
 ax1 = plt.subplot(211)
 box = ax1.get_position()
-ax1.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+ax1.set_position([box.x0, box.y0, box.width, box.height * 0.8])
 ax2 = plt.subplot(212)
 box = ax2.get_position()
-ax2.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+ax2.set_position([box.x0, box.y0, box.width, box.height * 0.9])
 
 
 otrs, rs, cs, otrsSum, rsSum, csSum, ys = ([] for _ in range(7))
@@ -44,19 +44,19 @@ while True:
             ax2.clear()
 
             ax1.set_ylim((0, 180))
-            ax1.set_title('Redirection amounts over time')
+            ax1.set_title('Redirection amounts over time', y=1.35)
             ax1.set_ylabel('Redirection Applied to User (degrees)')
-            ax1.stackplot(ys, otrsSum, rsSum, csSum, labels=('Over time\nrotation', 'Rotational', 'Curvature'), colors="rgb")
+            ax1.stackplot(ys, otrsSum, rsSum, csSum, labels=('Over time rotation', 'Rotational', 'Curvature'), colors="rgb")
             # ax1.plot(ys[-60:], hybrid[-60:], color='b', label='Hybrid', linewidth=0.5)
 
             # ax2.set_xticks(list(map(int, ys[::10])))
             # ax2.set_xlim((ys[-1] - 30, ys[-1]))
             ax2.set_xlabel('Time from start (seconds)', loc='right')
             ax2.set_ylabel('Redirection per Second (degrees/s)')
-            for series, color, label in zip([otrs, rs, cs], "rgb", ["Over Time\nRotation", "Rotational", "Curvature"]):
+            for series, color, label in zip([otrs, rs, cs], "rgb", ["Over Time Rotation", "Rotational", "Curvature"]):
                  ax2.plot(ys, series, color=color, label=label, linewidth=1)
 
 
-            for ax in (ax1, ax2):
-                ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Redirection source")
+            for ax in (ax1,):
+                ax.legend(loc='lower left', bbox_to_anchor=(0, 1, 1, 0), mode="expand", ncols=3, title="Redirection source")
             plt.pause(0.05)
