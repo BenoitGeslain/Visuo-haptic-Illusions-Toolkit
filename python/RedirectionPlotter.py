6457c3ad1501c50d732575495e35847abc341a6d
bjrtx <57685415+bjrtx@users.noreply.github.com>
2024-03-02 20:10:23 +0100

    Start localizing Python window

--- python/RedirectionPlotter.py
+++ python/RedirectionPlotter.py
@@ -6,6 +6,10 @@ import re
 import matplotlib
 matplotlib.use('TkAgg')
 
+import gettext
+gettext.translation('messages', localedir='./locales',languages=['fr']).install()
+
+
 plt.ion() # turn on interactive mode
 fig = plt.figure(figsize=(10,6), constrained_layout=True)
 ax1 = plt.subplot(211)
@@ -27,7 +31,7 @@ serversocket.listen(5)
 
 while True:
     print("Waiting for socket")
-    clientsocket, _ = serversocket.accept()
+    clientsocket, __ = serversocket.accept()
     with clientsocket:
         while True:
             try:
@@ -44,19 +48,20 @@ while True:
             ax2.clear()
 
             ax1.set_ylim((0, 180))
-            ax1.set_title('Redirection amounts over time', y=1.35)
-            ax1.set_ylabel('Redirection Applied to User (degrees)')
-            ax1.stackplot(ys, otrsSum, rsSum, csSum, labels=('Over time rotation', 'Rotational', 'Curvature'), colors="rgb")
+            ax1.set_title(_('Redirection amounts over time'), y=1.35)
+            ax1.set_ylabel(_('Redirection Applied to User (degrees)'))
+            ax1.stackplot(ys, otrsSum, rsSum, csSum, labels=(_('Over time rotation'), _('Rotational'), _('Curvature')), colors="rgb")
             # ax1.plot(ys[-60:], hybrid[-60:], color='b', label='Hybrid', linewidth=0.5)
 
             # ax2.set_xticks(list(map(int, ys[::10])))
             # ax2.set_xlim((ys[-1] - 30, ys[-1]))
-            ax2.set_xlabel('Time from start (seconds)', loc='right')
-            ax2.set_ylabel('Redirection per Second (degrees/s)')
-            for series, color, label in zip([otrs, rs, cs], "rgb", ["Over Time Rotation", "Rotational", "Curvature"]):
+            ax2.set_xlabel(_('Time from start (seconds)'), loc='right')
+            ax2.set_ylabel(_('Redirection per Second (degrees/s)'))
+            for series, color, label in zip([otrs, rs, cs], "rgb", [_("Over Time Rotation"), _("Rotational"), _("Curvature")]):
                  ax2.plot(ys, series, color=color, label=label, linewidth=1)
 
 
             for ax in (ax1,):
-                ax.legend(loc='lower left', bbox_to_anchor=(0, 1, 1, 0), mode="expand", ncols=3, title="Redirection source")
+                ax.legend(loc='lower left', bbox_to_anchor=(0, 1, 1, 0), mode="expand", ncols=3, title=_("Redirection source"))
             plt.pause(0.05)
+
