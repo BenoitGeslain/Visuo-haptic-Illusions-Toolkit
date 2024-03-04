0fbda06660d17327db04f9c346dce83f09798b43
BenoitGeslain <benoitgeslain@gmail.com>
2024-03-04 10:31:26 +0000

    Deploying to gh-pages from @ BenoitGeslain/Visuo-haptic-Illusions-Toolkit@0462c93db97faaf0d22434df13515484b2c555e0 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -3,5 +3,5 @@ var searchData=
   ['calibrate_248',['Calibrate',['../classVHToolkit_1_1Calibration_1_1SceneCalibration.html#a07045e508620ef2a903359bfdd970bd3',1,'VHToolkit::Calibration::SceneCalibration']]],
   ['copyheadandlimbtransform_249',['CopyHeadAndLimbTransform',['../classVHToolkit_1_1Redirection_1_1WorldRedirection_1_1WorldRedirectionTechnique.html#aeb7c04cb4ec281a3920a03d81f4f1ba1',1,'VHToolkit::Redirection::WorldRedirection::WorldRedirectionTechnique']]],
   ['createnewfile_250',['CreateNewFile',['../classVHToolkit_1_1Logging_1_1JsonLogging.html#ac947a9cc29ad451cf42c35a39855f158',1,'VHToolkit.Logging.JsonLogging.CreateNewFile()'],['../classVHToolkit_1_1Logging_1_1Logging.html#a1f3aaeabf70901177f1e871be4a3a395',1,'VHToolkit.Logging.Logging.CreateNewFile()']]],
-  ['curvatureradiustorotationrate_251',['CurvatureRadiusToRotationRate',['../classVHToolkit_1_1Redirection_1_1WorldRedirection_1_1Razzaque2001Curvature.html#aa6c6750762fd8e7688123e2315cf2731',1,'VHToolkit::Redirection::WorldRedirection::Razzaque2001Curvature']]]
+  ['curvatureradiustorotationrate_251',['CurvatureRadiusToRotationRate',['../classVHToolkit_1_1Redirection_1_1WorldRedirection_1_1Razzaque2001Curvature.html#a26f16772d8807b171de440fa25ca5d87',1,'VHToolkit::Redirection::WorldRedirection::Razzaque2001Curvature']]]
 ];
