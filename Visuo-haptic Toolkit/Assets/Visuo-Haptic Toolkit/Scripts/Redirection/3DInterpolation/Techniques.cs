2db7df350a21d24f68caaaa1d0802ceed0eff22a
bjrtx <57685415+bjrtx@users.noreply.github.com>
2024-04-27 21:47:10 +0200

    Add automatic BB to Kohli2010RedirectedTouching

--- Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Redirection/3DInterpolation/Techniques.cs
+++ Visuo-haptic Toolkit/Assets/Visuo-Haptic Toolkit/Scripts/Redirection/3DInterpolation/Techniques.cs
@@ -1,5 +1,7 @@
 using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 using VHToolkit.Redirection.BodyRedirection;
@@ -12,14 +14,32 @@ namespace VHToolkit.Redirection.Interpolation3D {
 	public class Kohli2010RedirectedTouching : BodyRedirectionTechnique {
 
 		Func<Vector3, Vector3> displace = null;
+		bool add_boundaries = true;
 
 		/// <summary>
 		/// Compute the displacement vector field, which should be done only on init and whenever its parameters change for efficiency reasons.
 		/// </summary>
 		public void ComputeDisplacement(Scene scene) {
+			var x = Array.ConvertAll(scene.referenceParent.GetComponentsInChildren<Transform>(), t => t.position);
+			var y = Array.ConvertAll(scene.interpolatedParent.GetComponentsInChildren<Transform>(), t => t.position);
+			if (add_boundaries) {
+				var bounds = new Bounds(x[0], Vector3.zero);
+				foreach (var v in x.Concat(y)) {
+					bounds.Encapsulate(v);
+				}
+				bounds.extents *= 2f;
+				Vector3[] ends = { bounds.min, bounds.max };
+				var supplementary_fixed = new List<Vector3>(8);
+				foreach (var a in ends)
+					foreach (var b in ends)
+						foreach (var c in ends)
+							supplementary_fixed.Add(new Vector3(a.x, b.y, c.z));
+				x = x.Concat(supplementary_fixed).ToArray();
+				y = y.Concat(supplementary_fixed).ToArray();
+			}
 			displace = ThinPlateSpline.SabooSmoothedDisplacementField(
-				Array.ConvertAll(scene.referenceParent.GetComponentsInChildren<Transform>(), t => t.position),
-				Array.ConvertAll(scene.interpolatedParent.GetComponentsInChildren<Transform>(), t => t.position),
+				x,
+				y,
 				scene.parameters.SmoothingParameter,
 				scene.parameters.Rescale
 			);
